8421f834b366f1e3c4add1249d3a7ccf2d4c8c93
Bharatchandran <bharatchandrankr@gmail.com>
2021-11-09 10:33:07 +0530

    Added Router Supprt for App

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 import "./styles/index.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
